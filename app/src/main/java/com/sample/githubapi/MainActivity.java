7eb7c28f2b0a8bc81ec385843e832a8aa86ac4e2
nimmanimaruthi <maruthi.nimmani24@gmail.com>
2021-10-16 22:17:33 -0500

    Initialized the views

--- app/src/main/java/com/sample/githubapi/MainActivity.java
+++ app/src/main/java/com/sample/githubapi/MainActivity.java
@@ -1,14 +1,32 @@
 package com.sample.githubapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView receyclerView;
+    private ProgressBar progressBar;
+    private TextView txtMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialise();
+        setUpViewModel();
+    }
+
+    private void setUpViewModel() {
+
+    }
+
+    private void initialise() {
+        receyclerView = findViewById(R.id.recyclerView);
+        txtMessage = findViewById(R.id.txtMessage);
+        progressBar = findViewById(R.id.progressBar);
     }
 }
