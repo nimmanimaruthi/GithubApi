5855810678308417abf408a56e28744ac096a6ac
nimmanimaruthi <maruthi.nimmani24@gmail.com>
2021-10-17 00:16:15 -0500

    Application class is created and defined in androidManifest.xml

---
+++ app/src/main/java/com/sample/githubapi/GithubApplication.java
@@ -0,0 +1,11 @@
+package com.sample.githubapi;
+
+import android.app.Application;
+
+public class GithubApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
