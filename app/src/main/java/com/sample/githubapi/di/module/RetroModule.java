b4796f6201e2fd23eecbcbcf19d6ba87ef9c94ec
nimmanimaruthi <maruthi.nimmani24@gmail.com>
2021-10-16 23:41:39 -0500

    Internet Permission Added in Mainfest.xml Creted Instance of APiService in RetroModule

--- app/src/main/java/com/sample/githubapi/di/module/RetroModule.java
+++ app/src/main/java/com/sample/githubapi/di/module/RetroModule.java
@@ -1,15 +1,21 @@
 package com.sample.githubapi.di.module;
 
+import com.sample.githubapi.apiservice.ApiService;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroModule {
-    private String BASE_URL = "https://api.github.com/repos/";
+    private static String BASE_URL = "https://api.github.com/repos/";
 
-    private Retrofit getRetrofitInstance() {
+    public static Retrofit getRetrofitInstance() {
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+    }
+
+    public static ApiService provideApiService(Retrofit retrofit) {
+        return retrofit.create(ApiService.class);
     }
 }
