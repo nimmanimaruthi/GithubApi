4c7abe8ebf688df6b38641fc2df8f5dfa1440f82
nimmanimaruthi <maruthi.nimmani24@gmail.com>
2021-10-16 22:56:04 -0500

    ViewModel is initialsed in MainActivity and Observer attached to listen for data change

--- app/src/main/java/com/sample/githubapi/MainActivity.java
+++ app/src/main/java/com/sample/githubapi/MainActivity.java
@@ -1,17 +1,25 @@
 package com.sample.githubapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.sample.githubapi.model.CommitRecord;
+import com.sample.githubapi.viewmodel.MainActivityViewModel;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView receyclerView;
     private ProgressBar progressBar;
     private TextView txtMessage;
+    private MainActivityViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +29,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpViewModel() {
+        viewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication()))
+                .get(MainActivityViewModel.class);
 
+        viewModel.registerObserver().observe(this, commitRecords -> {
+            if (commitRecords == null || commitRecords.isEmpty()) {
+                // TODO display error message
+            } else {
+                // TODO display data on receylerview
+            }
+        });
     }
 
     private void initialise() {
