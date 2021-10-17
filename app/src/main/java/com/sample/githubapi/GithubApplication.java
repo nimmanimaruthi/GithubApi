221d5d5d56c6e5fd231ebc0eda41711f275aee78
nimmanimaruthi <maruthi.nimmani24@gmail.com>
2021-10-17 00:18:17 -0500

    DaggerAppComponent is now created in application class.

--- app/src/main/java/com/sample/githubapi/GithubApplication.java
+++ app/src/main/java/com/sample/githubapi/GithubApplication.java
@@ -2,10 +2,20 @@ package com.sample.githubapi;
 
 import android.app.Application;
 
+import com.sample.githubapi.di.AppComponent;
+import com.sample.githubapi.di.DaggerAppComponent;
+
 public class GithubApplication extends Application {
+
+    private static AppComponent appComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        appComponent = DaggerAppComponent.create();
+    }
+
+    public static AppComponent getAppComponent() {
+        return appComponent;
     }
 }
