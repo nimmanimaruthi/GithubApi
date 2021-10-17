e36ce56ee80fd79909c18fbae55e2d18533f7833
nimmanimaruthi <maruthi.nimmani24@gmail.com>
2021-10-17 00:20:35 -0500

    ApiService injected in the ViewModel.

--- app/src/main/java/com/sample/githubapi/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/sample/githubapi/viewmodel/MainActivityViewModel.java
@@ -1,12 +1,14 @@
 package com.sample.githubapi.viewmodel;
 
-import android.app.Application;
 import android.util.Log;
 
-import com.sample.githubapi.di.module.RetroModule;
+import com.sample.githubapi.GithubApplication;
+import com.sample.githubapi.apiservice.ApiService;
 import com.sample.githubapi.model.CommitRecord;
 
 import java.util.List;
+
+import javax.inject.Inject;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -16,22 +18,18 @@ import retrofit2.Response;
 
 public class MainActivityViewModel extends ViewModel {
     private String TAG = MainActivityViewModel.class.getSimpleName();
-    private Application application;
-
-    public MainActivityViewModel() {
-        //this.application = application;
-    }
+    @Inject
+    ApiService apiService;
 
     private MutableLiveData<List<CommitRecord>> records = new MutableLiveData<>();
 
     public MutableLiveData<List<CommitRecord>> registerObserver() {
+        GithubApplication.getAppComponent().inject(this);
         return records;
     }
 
     public void fetchCommitRecords() {
-        // TODO network call to fetch data
-        RetroModule.provideApiService(RetroModule.getRetrofitInstance())
-                .getCommitRecords("nimmanimaruthi", "GithubApi")
+        apiService.getCommitRecords("nimmanimaruthi", "GithubApi")
                 .enqueue(new Callback<List<CommitRecord>>() {
                     @Override
                     public void onResponse(Call<List<CommitRecord>> call, Response<List<CommitRecord>> response) {
