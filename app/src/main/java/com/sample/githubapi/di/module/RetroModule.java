7b7ec4707fb0cc4af0af6f3d9cf9863442701638
nimmanimaruthi <maruthi.nimmani24@gmail.com>
2021-10-17 00:09:59 -0500

    Retrofit instances will be provided by Dagger, so included @Provides and @Module Annotations to method.

--- app/src/main/java/com/sample/githubapi/di/module/RetroModule.java
+++ app/src/main/java/com/sample/githubapi/di/module/RetroModule.java
@@ -2,12 +2,19 @@ package com.sample.githubapi.di.module;
 
 import com.sample.githubapi.apiservice.ApiService;
 
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+@Module
 public class RetroModule {
     private static String BASE_URL = "https://api.github.com/repos/";
 
+    @Singleton
+    @Provides
     public static Retrofit getRetrofitInstance() {
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -15,6 +22,8 @@ public class RetroModule {
                 .build();
     }
 
+    @Singleton
+    @Provides
     public static ApiService provideApiService(Retrofit retrofit) {
         return retrofit.create(ApiService.class);
     }
